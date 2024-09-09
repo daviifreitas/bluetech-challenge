f1c04b270a72ceae5452486ba9fd30de37228516
Davi Freitas <davimello14@gmail.com>
2024-09-09 16:45:46 -0300

    [Refactor] add globalUsing for test project

---
+++ Service/Schedule/Schedule.Test/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using AutoFixture;
+global using Bogus;
+global using BuildingBlocks.Exceptions;
+global using FakeItEasy;
+global using FluentAssertions;
+global using Schedule.Application.Schedule.Command.CreateSchedule;
+global using Schedule.Domain.Interfaces.Service;
