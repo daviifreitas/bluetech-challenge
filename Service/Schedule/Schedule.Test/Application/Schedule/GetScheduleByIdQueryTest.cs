957125a2b9657680274fbdee1ac5280866791fda
Davi Freitas <davimello14@gmail.com>
2024-09-10 01:24:04 -0300

    [Add] get schedule handler unit tests

---
+++ Service/Schedule/Schedule.Test/Application/Schedule/GetScheduleByIdQueryTest.cs
@@ -0,0 +1,85 @@
+﻿using Schedule.Application.Dto;
+using Schedule.Application.Exceptions;
+using Schedule.Application.Schedule.Query.GetScheduleById;
+
+namespace Schedule.Test.Application.Schedule;
+
+public class GetScheduleByIdQueryTest
+{
+    private readonly IFixture _fixture = new Fixture();
+    private readonly Faker _faker = new();
+    private readonly IScheduleService _scheduleService = A.Fake<IScheduleService>();
+    private readonly GetScheduleByIdQueryHandler _handler;
+    
+    public GetScheduleByIdQueryTest()
+    {
+        _handler = new GetScheduleByIdQueryHandler(_scheduleService);
+    }
+    
+    [Fact]
+    public async void Handle_ExistScheduleById_ShouldReturnExpectedData()
+    {
+        GetScheduleByIdQuery query = _fixture.Create<GetScheduleByIdQuery>();
+        Domain.Entities.Schedule schedule = _fixture.Create<Domain.Entities.Schedule>();
+        ScheduleDto expectedSchedule = new(schedule.Id, schedule.Name, schedule.Email, schedule.PhoneNumber);
+        
+        A.CallTo(() => _scheduleService.GetByIdAsync(A<int>.That.Matches(x => x == query.Id)))
+            .Returns(schedule);
+        
+        GetScheduleByIdResult getScheduleByIdResult = await _handler.Handle(query, CancellationToken.None);
+        
+        getScheduleByIdResult.Result.Should().BeEquivalentTo(expectedSchedule);
+    }
+    
+    [Fact]
+    public async void Handle_NotExistScheduleById_ShouldThrows()
+    {
+        GetScheduleByIdQuery query = _fixture.Create<GetScheduleByIdQuery>();
+        Domain.Entities.Schedule? schedule = null;
+        
+        A.CallTo(() => _scheduleService.GetByIdAsync(A<int>.That.Matches(x => x == query.Id)))
+            .Returns(schedule);
+        
+        Func<Task> act = async () => await _handler.Handle(query, CancellationToken.None);
+        
+        await act.Should().ThrowAsync<ScheduleNotFoundException>().WithMessage($"Entity \"Schedule\" ({query.Id} was not found.)");
+    }
+    
+    [Fact]
+    public async void Handle_ThrowsAnyException_ShouldThrows()
+    {
+        GetScheduleByIdQuery query = _fixture.Create<GetScheduleByIdQuery>();
+        
+        A.CallTo(() => _scheduleService.GetByIdAsync(A<int>.That.Matches(x => x == query.Id)))
+            .Throws<Exception>();
+        
+        Func<Task> act = async () => await _handler.Handle(query, CancellationToken.None);
+        
+        await act.Should().ThrowAsync<Exception>();
+    }
+    
+    [Fact]
+    public async void Validator_InvalidId_ShouldFail()
+    {
+        GetScheduleByIdQuery query = _fixture.Build<GetScheduleByIdQuery>().With(x => x.Id, 0).Create();
+        
+        GetScheduleByIdQueryValidator validator = new();
+        
+        FluentValidation.Results.ValidationResult validationResult = await validator.ValidateAsync(query);
+        
+        validationResult.IsValid.Should().BeFalse();
+        validationResult.Errors.Should().Contain(error => error.ErrorMessage == "Id is required.");
+    }
+    
+    [Fact]
+    public async void Validator_ValidId_ShouldPass()
+    {
+        GetScheduleByIdQuery query = _fixture.Create<GetScheduleByIdQuery>();
+        
+        GetScheduleByIdQueryValidator validator = new();
+        
+        FluentValidation.Results.ValidationResult validationResult = await validator.ValidateAsync(query);
+        
+        validationResult.IsValid.Should().BeTrue();
+    }
+}
---
+++ Service/Schedule/Schedule.Test/Application/Schedule/GetScheduleQueryTest.cs
@@ -0,0 +1,66 @@
+﻿using BuildingBlocks.Entity;
+using Schedule.Application.Dto;
+using Schedule.Application.Schedule.Query.GetSchedule;
+
+namespace Schedule.Test.Application.Schedule;
+
+public class GetScheduleQueryTest
+{
+    private readonly IFixture _fixture = new Fixture();
+    private readonly Faker _faker = new();
+    private readonly IScheduleService _scheduleService = A.Fake<IScheduleService>();
+    private readonly GetScheduleQueryHandler _handler;
+
+    public GetScheduleQueryTest()
+    {
+        _handler = new GetScheduleQueryHandler(_scheduleService);
+    }
+
+    [Fact]
+    public async void Handle_CorrectScheduleData_ShouldReturnExpectedData()
+    {
+        GetScheduleQuery query = _fixture.Create<GetScheduleQuery>();
+        IEnumerable<Domain.Entities.Schedule> schedules = _fixture.CreateMany<Domain.Entities.Schedule>();
+        IEnumerable<ScheduleDto> expectedSchedules =
+            schedules.Select(x => new ScheduleDto(x.Id, x.Name, x.Email, x.PhoneNumber));
+
+        A.CallTo(() => _scheduleService.GetManyAsDynamicTypeByFilterAsync(A<Func<Domain.Entities.Schedule, bool>>._,
+                A<Func<Domain.Entities.Schedule, ScheduleDto>>._))
+            .Returns(expectedSchedules);
+
+        GetScheduleResult getScheduleResult = await _handler.Handle(query, CancellationToken.None);
+
+        getScheduleResult.Result.Should().BeEquivalentTo(expectedSchedules);
+    }
+
+    [Fact]
+    public async void Handle_EmptyScheduleData_ShouldReturnEmptyData()
+    {
+        GetScheduleQuery query = _fixture.Create<GetScheduleQuery>();
+        IEnumerable<Domain.Entities.Schedule> schedules = Enumerable.Empty<Domain.Entities.Schedule>();
+        IEnumerable<ScheduleDto> expectedSchedules =
+            schedules.Select(x => new ScheduleDto(x.Id, x.Name, x.Email, x.PhoneNumber));
+
+        A.CallTo(() => _scheduleService.GetManyAsDynamicTypeByFilterAsync(A<Func<Domain.Entities.Schedule, bool>>._,
+                A<Func<Domain.Entities.Schedule, ScheduleDto>>._))
+            .Returns(expectedSchedules);
+
+        GetScheduleResult getScheduleResult = await _handler.Handle(query, CancellationToken.None);
+
+        getScheduleResult.Result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async void Handle_ThrowsAnyException_ShouldThrows()
+    {
+        GetScheduleQuery? query = _fixture.Create<GetScheduleQuery>();
+
+        A.CallTo(() => _scheduleService.GetManyAsDynamicTypeByFilterAsync(A<Func<Domain.Entities.Schedule, bool>>._,
+                A<Func<Domain.Entities.Schedule, ScheduleDto>>._))
+            .Throws<Exception>();
+
+        Func<Task> act = async () => await _handler.Handle(query, CancellationToken.None);
+
+        await act.Should().ThrowAsync<Exception>();
+    }
+}
